74561dda95242b085a857b56078d175b3b02234c
Victor Meneses <menesesvictor56@gmail.com>
2022-05-23 21:54:50 -0300

    Envio de Arquivos Descrevendo Aprendizado

---
+++ principaiscomandos.js
@@ -0,0 +1,34 @@
+/*
+    touch nomedoarquivo.extensão
+    cria arquivo no diretório
+
+    code . nomedoarquivo.extensão
+    abre o arquivo no editor
+
+    git init cria o diretório
+
+    cd nomedapasta/
+    acessa a pasta
+
+    git add . 
+    adiciona todos os arquivos do diretório para serem comitados
+
+
+    git commit -m "DESCRICAO"
+    faz o envio dos arquivos 
+
+    git remote add origin LINK DO PROJETO NO GITHUB
+
+    git push -u origin BRANCHPRINCIPAL
+    envia os arquivos para o projeto no website
+
+    git pull origin master
+    baixa os arquivos do projeto no diretório local
+
+
+    touch .gitignore 
+    para adicionar arquivos dentro do arquivo .gitignore e eles não serão enviados a pasta de projeto no site do github
+
+
+
+*/
